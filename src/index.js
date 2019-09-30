5d6a34f9b6220ee471ba15ee667ed6cfc2952fbe
sue <sue@MacBook-Pro-de-sue.local>
2019-09-29 20:13:50 -0500

    getting all the challenges

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters';
+//const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters';
+const API = 'https://rickandmortyapi.com/api/character/';
+window.onload = localStorage.clear()   
 
 const getData = api => {
     fetch(api)
@@ -18,26 +20,40 @@ const getData = api => {
     `
       }).join('');
       let newItem = document.createElement('section');
-      newItem.classList.add('Items');
+      newItem.classList.add('Items'); 
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
     .catch(error => console.log(error));
 }
 
-  function loadData(){
-  const nextUrl = localStorage.getItem('next_fetch');
-  if(nextUrl){
-    getData(nextUrl);
-  } else {
-    getData(API)
-  }
+const loadData = async () => {
+    const nextUrl = localStorage.getItem('next_fetch');
+    if(nextUrl){
+      await getData(nextUrl);
+   } 
+    if(nextUrl === null){
+      await getData(API); 
+    }
+    if(nextUrl === ""){
+      creatingEndMessage(); 
+      intersectionObserver.unobserve($observe);
+    }
+}
+const creatingEndMessage = () => {
+    let messageContainer = document.createElement('div'); 
+    messageContainer.classList.add('LastMessage_container');
+    let messageElement = document.createElement('p'); 
+    let messageText = document.createTextNode('uppps, there are no more images to load.')
+    let text = messageElement.appendChild(messageText);
+    messageContainer.appendChild(text)
+    $app.appendChild(messageContainer);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
   if (entries[0].isIntersecting) {
-    loadData();
-  }
+    loadData(); 
+  } 
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
